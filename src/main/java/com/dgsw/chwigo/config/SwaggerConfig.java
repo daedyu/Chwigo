a8a207786973ec4bd31e47a514859b4ab1cf792c
kmg <legolove08@naver.com>
2026-05-17 11:30:00 +0900

    feat: Swagger API 문서 설정

---
+++ src/main/java/com/dgsw/chwigo/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.dgsw.chwigo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String BEARER_AUTH = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Chwigo API")
+                        .description("자취생을 위한 공동구매 서비스 API")
+                        .version("v1.0.0")
+                        .contact(new Contact().name("chwigo").email("admin@chwigo.com")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .name(BEARER_AUTH)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("로그인 후 발급받은 AccessToken을 입력하세요 (Bearer 제외)")));
+    }
+}
